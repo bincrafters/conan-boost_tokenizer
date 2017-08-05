32ea7116b481780bc1314e5bfce76c439b874080
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-05 11:47:18 -0400

    fix include path and other changes

--- conanfile.py
+++ conanfile.py
@@ -22,7 +22,7 @@ class BoostTokenizerConan(ConanFile):
 
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
-        self.copy(pattern="*", dst="", src=include_dir)
+        self.copy(pattern="*", dst="include", src=include_dir)
 
     def package_id(self):
         self.info.header_only()
