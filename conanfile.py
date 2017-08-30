9330d8291cf54d49d30ff1aa0080a5bfda162ff3
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-30 12:27:23 -0400

    Fixed source methods and a few package methods

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,6 @@ class BoostTokenizerConan(ConanFile):
     version = "1.64.0"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-tokenizer"
-    source_url = "https://github.com/boostorg/tokenizer"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["tokenizer"]
@@ -18,9 +17,12 @@ class BoostTokenizerConan(ConanFile):
                       #assert1 config0 iterator5 mpl5 throw_exception2
 
     def source(self):
+        boostorg_github = "https://github.com/boostorg"
+        archive_name = "boost-" + self.version  
         for lib_short_name in self.lib_short_names:
-            self.run("git clone --depth=1 --branch=boost-{0} https://github.com/boostorg/{1}.git"
-                     .format(self.version, lib_short_name)) 
+            tools.get("{0}/{1}/archive/{2}.tar.gz"
+                .format(boostorg_github, lib_short_name, archive_name))
+            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
 
     def package(self):
         for lib_short_name in self.lib_short_names:
