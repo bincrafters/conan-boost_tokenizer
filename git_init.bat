5b1bb76c0eaeb1ce7f570694e32bc0f89741338a
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 12:06:55 -0400

    Removed old setup scripts

--- git_init.bat
+++
@@ -1,6 +0,0 @@
-git init
-git remote add origin git@github.com:bincrafters/conan-boost-tokenizer.git
-git fetch
-git add .
-git commit -m "initial commit"
-git push --set-upstream origin master
--- upload.sh
+++
@@ -1,1 +0,0 @@
-conan upload --all --remote bincrafters Boost.Tokenizer/1.64.0@bincrafters/testing
