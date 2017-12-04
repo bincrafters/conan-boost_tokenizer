5c4631c7236d346a2411202210afdabdcb181c33
Rene Rivera <grafikrobot@gmail.com>
2017-12-03 23:37:30 -0600

    Next generation of Boost packages.

--- conanfile.py
+++ conanfile.py
@@ -1,34 +1,40 @@
 from conans import ConanFile, tools
-import os
+
 
 class BoostTokenizerConan(ConanFile):
     name = "Boost.Tokenizer"
     version = "1.65.1"
-    short_paths = True
     url = "https://github.com/bincrafters/conan-boost-tokenizer"
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
+
+    requires = \
+        "Boost.Assert/1.65.1@bincrafters/testing", \
+        "Boost.Config/1.65.1@bincrafters/testing", \
+        "Boost.Iterator/1.65.1@bincrafters/testing", \
+        "Boost.Mpl/1.65.1@bincrafters/testing", \
+        "Boost.Throw_Exception/1.65.1@bincrafters/testing"
+
     lib_short_names = ["tokenizer"]
-    requires =  "Boost.Assert/1.65.1@bincrafters/testing", \
-                      "Boost.Config/1.65.1@bincrafters/testing", \
-                      "Boost.Iterator/1.65.1@bincrafters/testing", \
-                      "Boost.Mpl/1.65.1@bincrafters/testing", \
-                      "Boost.Throw_Exception/1.65.1@bincrafters/testing"
+    is_header_only = True
 
-                      #assert1 config0 iterator5 mpl5 throw_exception2
+    # BEGIN
 
-    def source(self):
-        boostorg_github = "https://github.com/boostorg"
-        archive_name = "boost-" + self.version  
-        for lib_short_name in self.lib_short_names:
-            tools.get("{0}/{1}/archive/{2}.tar.gz"
-                .format(boostorg_github, lib_short_name, archive_name))
-            os.rename(lib_short_name + "-" + archive_name, lib_short_name)
-
-    def package(self):
-        for lib_short_name in self.lib_short_names:
-            include_dir = os.path.join(lib_short_name, "include")
-            self.copy(pattern="*", dst="include", src=include_dir)		
+    short_paths = True
+    build_requires = "Boost.Generator/1.65.1@bincrafters/testing"
 
     def package_id(self):
-        self.info.header_only()
+        if self.is_header_only:
+            self.info.header_only()
+
+    @property
+    def env(self):
+        try:
+            with tools.pythonpath(super(self.__class__, self)):
+                import boostgenerator # pylint: disable=F0401
+                boostgenerator.BoostConanFile(self)
+        except:
+            pass
+        return super(self.__class__, self).env
+
+    # END
