cd99d10d91151c9d8408b10079321692b4e1f35d
Jerry W <jerrywiltse@gmail.com>
2018-02-04 00:01:04 -0500

    change ver to 1.65.1 after merge

--- conanfile.py
+++ conanfile.py
@@ -6,7 +6,7 @@ from conans import ConanFile, tools
 
 class BoostTokenizerConan(ConanFile):
     name = "boost_tokenizer"
-    version = "1.66.0"
+    version = "1.65.1"
     url = "https://github.com/bincrafters/conan-boost_tokenizer"
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
@@ -17,12 +17,12 @@ class BoostTokenizerConan(ConanFile):
         self.info.header_only()
 
     requires = (
-        "boost_package_tools/1.66.0@bincrafters/testing",
-        "boost_assert/1.66.0@bincrafters/testing",
-        "boost_config/1.66.0@bincrafters/testing",
-        "boost_iterator/1.66.0@bincrafters/testing",
-        "boost_mpl/1.66.0@bincrafters/testing",
-        "boost_throw_exception/1.66.0@bincrafters/testing"
+        "boost_package_tools/1.65.1@bincrafters/testing",
+        "boost_assert/1.65.1@bincrafters/testing",
+        "boost_config/1.65.1@bincrafters/testing",
+        "boost_iterator/1.65.1@bincrafters/testing",
+        "boost_mpl/1.65.1@bincrafters/testing",
+        "boost_throw_exception/1.65.1@bincrafters/testing"
     )
 
     # BEGIN
@@ -30,7 +30,7 @@ class BoostTokenizerConan(ConanFile):
     description = "Please visit http://www.boost.org/doc/libs/1_66_0"
     license = "BSL-1.0"
     short_paths = True
-    build_requires = "boost_generator/1.66.0@bincrafters/testing"
+    build_requires = "boost_generator/1.65.1@bincrafters/testing"
 
     def package_id(self):
         getattr(self, "package_id_additional", lambda:None)()
