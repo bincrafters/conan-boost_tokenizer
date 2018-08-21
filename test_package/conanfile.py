dc10d0213bf116ceaefd87a4b0e80e98a20182c1
Jerry W <jerrywiltse@gmail.com>
2018-08-20 21:57:46 -0400

    use generic test_package conanfile

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
 
 from conans import ConanFile, CMake, tools, RunEnvironment
 import os
@@ -9,22 +10,10 @@ class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
 
-    def requirements(self):
-        boost_deps = ['assert', 'config', 'iterator', 'mpl', 'throw_exception', 'tokenizer']
-        for lib in boost_deps:
-            self.requires("boost_" + lib + "/1.67.0@" + self.user + "/" + self.channel)
-        if False:
-            if not tools.os_info.is_windows:
-                self.requires("openmpi/3.0.0@bincrafters/stable")
-
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
     def test(self):
-        with tools.environment_append(RunEnvironment(self).vars):
-            if self.settings.os == "Windows":
-                self.run(os.path.join("bin", "test_package"))
-            else:
-                self.run("DYLD_LIBRARY_PATH=%s %s" % (os.environ['DYLD_LIBRARY_PATH'], os.path.join("bin", "test_package")))
+        self.run(os.path.join("bin", "test_package"), run_environment=True)
