6210a5c7cb65b88c181027181c9bf696eb903e29
Jerry W <jerrywiltse@gmail.com>
2018-12-13 13:38:51 -0500

    change boost base reference to stable

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.68.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/stable")
 
 class BoostTokenizerConan(base.BoostBaseConan):
     name = "boost_tokenizer"
