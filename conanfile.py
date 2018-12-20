7a47071bc9117894c2188e34e8eab451d987c790
Jerry W <jerrywiltse@gmail.com>
2018-12-20 02:13:54 -0500

    switch base dep to stable

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/1.69.0@bincrafters/stable")
 
 class BoostTokenizerConan(base.BoostBaseConan):
     name = "boost_tokenizer"
