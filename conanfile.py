745d2a2c271765574286ad4bc9f703f7ff4e4980
Jerry W <jerrywiltse@gmail.com>
2019-01-26 23:58:50 -0500

    re-implement version numbers, fix travis badges, refresh ci scripts, move to python 3.6 on ci

--- conanfile.py
+++ conanfile.py
@@ -8,6 +8,7 @@ base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostTokenizerConan(base.BoostBaseConan):
     name = "boost_tokenizer"
+    version = "1.68.0"
     url = "https://github.com/bincrafters/conan-boost_tokenizer"
     lib_short_names = ["tokenizer"]
     header_only_libs = ["tokenizer"]
